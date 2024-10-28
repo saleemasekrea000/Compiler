54fb727ebe9e3836e745d82063c72e1c5619e3d1
karam khaddour <k.khaddour@innopolis.university>
2024-10-28 18:29:30 +0300

    refactor print

--- src/parser/ast.cpp
+++ src/parser/ast.cpp
@@ -4,7 +4,46 @@
 #include <cstdio>
 #include <memory>
 #include <string>
+#include <unordered_map>
 
+ static const std::unordered_map<Node_Type,std::string> type_map = {
+            {PROGRAM, "Program"},
+            {DECLARATION, "Declarations"},
+            {SIMPLE_DECLARATION,"Simple declaration"},
+            {VARIABLE_DECLARATION, "Variable declaration"},
+            {PRIMARY_EXPRESSION, "Primary expresson"},
+            {PRIMARY_NODE,"Primary node"},
+            {ARRAY_ACCESS,"Array access"},
+            {RECORD_ACCESS,"Record access"},
+            {SUMMAND, "Summand"},
+            {FACTOR,"Factor"},
+            {SIMPLE,"Simple"},
+            {RELATION,"Relation"},
+            {EXPRESSION,"Expression"},
+            {TYPE_DECLARATION, "Type declaration"},
+            {ARRAY_TYPE, "Array type"},
+            {RECORD_TYPE, "Record type"},
+            {VARIABLE_DECLARATIONS,"Variable Declarations"},
+            {STATEMENT, "Statement"},
+            {ITERATION_STATEMENT, "Iteration statement"},
+            {WHILE_STATEMENT, "While statement"},
+            {BODY, "Body"},
+            {FOR_STATEMENT,"For statement"},
+            {RANGE_EX, "Range"},
+            {RANGE_REVERSE,"Range reverse"},
+            {ASSIGN_STATEMENT, "Assignment statement"},
+            {IF_STATEMENT,"if statement"},
+            {IF_STATEMENT_ELSE, "if and else statement"},
+            {JUMP_STATEMENT, "Jump Statement"},
+            {RETURN_EX, "Return"},
+            {CONTINUE_EX, "Continue"},
+            {BREAK_EX,"Break"},
+            {Routine_Call,"Routine Call"},
+            {Argument_Expression_List, "Arguments Expression List"},
+            {PARAMETER_DECLERATION, "Parameter Decleration"},
+            {PARAMETERS_EXPRESSION_LIST,"Parameters Expression List"},
+            {ROUTINE_DECLERATION,"Routine Decleration"}
+        };
 void print_ast(AST_Node* node, int indent) {
     if (!node) {
         return; // Base case: if node is null, do nothing
@@ -14,183 +53,47 @@ void print_ast(AST_Node* node, int indent) {
     for (int i = 0; i < indent; i++) {
         printf("  "); // Indentation for the current level
     }
-
-    // Print node type
-    switch (node->type) {
-        case PROGRAM:
-            printf("Program\n");
-            break;
-        case DECLARATION:
-            printf("Declarations\n");
-            break;
-        case SIMPLE_DECLARATION:
-            printf("Simple declaration\n");
-            break;
-        case PRIMARY_EXPRESSION :
-           printf("Primary expresson\n");
-           break;
-        case PRIMARY_NODE:
-           printf("Primary node\n");
-           break;
-        case VARIABLE_DECLARATION: {
-            printf("Variable declaration\n");
-            break;
-        }
-        case ARRAY_ACCESS: {
-            printf("Array access\n");
-            break;
-        }
-        case RECORD_ACCESS: {
-            printf("Record access\n");
-            break;
-        }
-        case SUMMAND: {
-            printf("Summand\n");
-            break;
-        }
-        case TYPE_DECLARATION: {
-            printf("Type declaration\n");
-            break;
-        }
-        case ARRAY_TYPE: {
-            printf("Array type\n");
-            break;
-        }
-        case RECORD_TYPE: {
-            printf("Record type\n");
-            break;
-        }
-        case FACTOR : {
-            printf("Factor\n");
-            break;
-        }
-        case SIMPLE : {
-            printf("Simple\n");
-            break;
-        }
-        case RELATION: {
-           printf("Relation\n");
-           break;
-        }
-        case EXPRESSION: {
-           printf("Expression\n");
-           break;
-        }
-        case STATEMENT: {
-            printf("Statement\n");
-            break;
-        }
-        case ITERATION_STATEMENT:{
-            printf("Iteration statement\n");
-            break;
-        }
-        case WHILE_STATEMENT:{
-            printf("While statement\n");
-            break;
-        }
-        case FOR_STATEMENT:{
-            printf("For statement\n");
-            break;
-        }
-        case IF_STATEMENT_ELSE:{
-            printf("if and else statement\n");
-            break;
-        } 
-        case IF_STATEMENT:{
-            printf("if statement\n");
-            break;
-        }
-        case ASSIGN_STATEMENT:{
-            printf("Assignment statement\n");
-            break;
-        }
-        case RANGE_EX:{
-            printf("Range\n");
-            break;
-        }
-        case RANGE_REVERSE:{
-            printf("Range reverse\n");
-            break;
-        }
-        case BODY:{
-            printf("Body\n");
-            break;
-        }
-        case CONTINUE_EX:{
-            printf("CONTINUE\n");
-            break;
-        }
-        case RETURN_EX:{
-            printf("Return\n");
-            break;
-        }
-        case JUMP_STATEMENT:{
-            printf("Jump Statement\n");
-            break;
-        }
-        case Argument_Expression_List: {
-            printf("Arguments Expression List\n");
-            break;
-        }
-        case VARIABLE_DECLARATIONS:{
-           printf("Variable Declarations\n");
-           break;
-        }
-        case Routine_Call:{
-            printf("Routine_Call\n");
-            break;
-        }
-        case BREAK_EX:{
-            printf("BREAK\n");
-            break;
-        }
-        case PARAMETER_DECLERATION:{
-            printf("PARAMETER_DECLERATION\n");
-            break;
-        }
-        case PARAMETERS_EXPRESSION_LIST:{
-            printf("PARAMETERS_EXPRESSION_LIST\n");
-            break;
-        }
-        case ROUTINE_DECLERATION:{
-            printf("ROUTINE_DECLERATION\n");
-            break;
-        }
-        case IDENTIFIER_NODE_TYPE:{
-          Identifier_Node* Identifier_node = static_cast<Identifier_Node*>(node);
-          printf("Identifier: %s\n", Identifier_node->identifier_name.c_str());
-          break;
-        }
-        case TYPE_NODE:{
-          Type_Node* type_node = static_cast<Type_Node*>(node);
-          printf("Type: %s\n", type_node->type_name.c_str());
-          break;
-        }
-         case BOOLEAN_NODE: {
-            Boolean_Node* boolean_node = static_cast<Boolean_Node*>(node);
-            std::cout << "Boolean : " << (boolean_node->val ? "true" : "false") << std::endl;
-            break;
-        }
-        case INTEGER_NODE: {
-            Integer_Node* integer_node = static_cast<Integer_Node*>(node);
-            std::cout << "Integer : " << integer_node->val << std::endl;
-            break;
-        }
-        case REAL_NODE: {
-            Real_Node* real_node = static_cast<Real_Node*>(node);
-            std::cout << "Real : " << real_node->val << std::endl;
-            break;
-        }
-        case OPERATOR: {
-            Operator* op_node = static_cast<Operator*>(node);
-            printf("Operator: %s\n", op_node->operation_name.c_str());
-            break;
-        }
-        default:
-            printf("Unknown Node Type\n");
-            break;
+    if(type_map.find(node->type)!=type_map.end()){
+        printf("%s\n", type_map.at(node->type).c_str());
     }
-  
+    else {
+        // Print node type
+        switch (node->type) {
+            case IDENTIFIER_NODE_TYPE:{
+            Identifier_Node* Identifier_node = static_cast<Identifier_Node*>(node);
+            printf("Identifier: %s\n", Identifier_node->identifier_name.c_str());
+            break;
+            }
+            case TYPE_NODE:{
+            Type_Node* type_node = static_cast<Type_Node*>(node);
+            printf("Type: %s\n", type_node->type_name.c_str());
+            break;
+            }
+            case BOOLEAN_NODE: {
+                Boolean_Node* boolean_node = static_cast<Boolean_Node*>(node);
+                std::cout << "Boolean : " << (boolean_node->val ? "true" : "false") << std::endl;
+                break;
+            }
+            case INTEGER_NODE: {
+                Integer_Node* integer_node = static_cast<Integer_Node*>(node);
+                std::cout << "Integer : " << integer_node->val << std::endl;
+                break;
+            }
+            case REAL_NODE: {
+                Real_Node* real_node = static_cast<Real_Node*>(node);
+                std::cout << "Real : " << real_node->val << std::endl;
+                break;
+            }
+            case OPERATOR: {
+                Operator* op_node = static_cast<Operator*>(node);
+                printf("Operator: %s\n", op_node->operation_name.c_str());
+                break;
+            }
+            default:
+                printf("Unknown Node Type\n");
+                break;
+        }
+    }
     // Recursively print each child node
     for (const auto& child : node->children) {
         print_ast(child, indent + 1); // Increase indentation for child nodes
