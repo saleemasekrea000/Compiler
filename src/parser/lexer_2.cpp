e0e29bd731750a4b2d3056bba213f8a5c6eebf05
Louay Farah <l.farah@innopolis.university>
2024-11-19 22:02:49 +0800

    Link lexer and parser

--- src/parser/lexer_2.cpp
+++ src/parser/lexer_2.cpp
@@ -1,112 +1,168 @@
-#include <fstream>
+#include <vector>
 #include <string>
-#include <sstream>
 #include <cstring>
-#include "lexer_2.hpp"
+#include "lexer.hpp"
 #include "ast.hpp"
 #include "grammar.tab.h"
 
-std::ifstream tokenFile;
+std::vector<Token> tokens;
+size_t current_token_index = 0;
 
 int yylex()
 {
-  std::string line;
-  if (!std::getline(tokenFile, line))
+  if (current_token_index >= tokens.size())
   {
-    // End of file, return 0 to indicate end of input
-    return 0;
+    return 0; // End of input
   }
 
-  // Parse the line to get the token type and value
-  std::string tokenType, tokenValue;
-  std::istringstream lineStream(line);
-  lineStream >> tokenType >> tokenValue;
-  // Return the appropriate token based on the token type
-  
-  if (tokenValue == "+=") return ADD_ASSIGN;
-  else if (tokenValue == "-=") return SUB_ASSIGN;
-  else if (tokenValue == "*=") return MUL_ASSIGN;
-  else if (tokenValue == "/=") return DIV_ASSIGN;
-  else if (tokenValue == "%=") return MOD_ASSIGN;
-  else if (tokenValue == "&&") return AND_OP;
-  else if (tokenValue == "||") return OR_OP;
-  else if (tokenValue == "<=") return LE_OP;
-  else if (tokenValue == ">=") return GE_OP;
-  else if (tokenValue == "!=") return NE_OP;
-  else if (tokenValue == ":=") return ASSIGN_OP;
-  else if (tokenValue == ";") return ';';
-  else if (tokenValue == "{") return '{';
-  else if (tokenValue == "}") return '}';
-  else if (tokenValue == ",") return ',';
-  else if (tokenValue == ":") return ':';
-  else if (tokenValue == "=") return '=';
-  else if (tokenValue == "(") return '(';
-  else if (tokenValue == ")") return ')';
-  else if (tokenValue == "[") return '[';
-  else if (tokenValue == "]") return ']';
-  else if (tokenValue == ".") return '.';
-  else if (tokenValue == "&") return '&';
-  else if (tokenValue == "!") return '!';
-  else if (tokenValue == "~") return '~';
-  else if (tokenValue == "-") return '-';
-  else if (tokenValue == "+") return '+';
-  else if (tokenValue == "*") return '*';
-  else if (tokenValue == "/") return '/';
-  else if (tokenValue == "%") return '%';
-  else if (tokenValue == "<") return '<';
-  else if (tokenValue == ">") return '>';
-  else if (tokenValue == "^") return '^';
-  else if (tokenValue == "|") return '|';
-  else if (tokenType == "IS") return IS;
-  else if (tokenType == "REVERSE") return REVERSE;
-  else if (tokenType == "WHILE") return WHILE;
-  else if (tokenType == "ROUTINE")return ROUTINE;
-  else if (tokenType == "END") return END;
-  else if (tokenType == "BREAK") return BREAK;
-  else if (tokenType == "RETURN") return RETURN;
-  else if (tokenType == "NOT") return NOT;
-  else if (tokenType== "XOR") return XOR;
-  else if (tokenType== "OR") return OR;
-  else if (tokenType=="AND") return AND;
-  else if (tokenType == "ELSE") return ELSE;
-  else if (tokenType=="THEN") return THEN;
-  else if (tokenType=="IF") return IF;
-  else if (tokenType=="IN") return IN;
-  else if (tokenType=="THEN") return THEN;
-  else if (tokenType=="FOR") return FOR;
-  else if(tokenType=="LOOP") return LOOP;
-  else if (tokenType == "ARRAY") return ARRAY;
-  else if (tokenType=="FALSE") return FALSE;
-  else if(tokenType=="TRUE") return TRUE;
-  else if(tokenType=="RECORD") return RECORD;
-  else if(tokenType=="TYPE") return TYPE;
-  else if (tokenType=="VAR") return VAR;
-  else if (tokenType=="INTEGER_LITERAL_KEYWORD") return INTEGER_LITERAL_KEYWORD;
-  else if (tokenType=="REAL_LITERAL_KEYWORD") return REAL_LITERAL_KEYWORD;
-  else if (tokenType=="BOOLEAN_LITERAL_KEYWORD") return BOOLEAN_LITERAL_KEYWORD;
-  else if (tokenType=="RANGE")return RANGE;
-  else if (tokenType=="CONTINUE")return CONTINUE;
-  else if (tokenType == "IDENTIFIER")
+  Token token = tokens[current_token_index++];
+  if (token.content == "+=")
+    return ADD_ASSIGN;
+  else if (token.content == "-=")
+    return SUB_ASSIGN;
+  else if (token.content == "*=")
+    return MUL_ASSIGN;
+  else if (token.content == "/=")
+    return DIV_ASSIGN;
+  else if (token.content == "%=")
+    return MOD_ASSIGN;
+  else if (token.content == "&&")
+    return AND_OP;
+  else if (token.content == "||")
+    return OR_OP;
+  else if (token.content == "<=")
+    return LE_OP;
+  else if (token.content == ">=")
+    return GE_OP;
+  else if (token.content == "!=")
+    return NE_OP;
+  else if (token.content == ":=")
+    return ASSIGN_OP;
+  else if (token.content == ";")
+    return ';';
+  else if (token.content == "{")
+    return '{';
+  else if (token.content == "}")
+    return '}';
+  else if (token.content == ",")
+    return ',';
+  else if (token.content == ":")
+    return ':';
+  else if (token.content == "=")
+    return '=';
+  else if (token.content == "(")
+    return '(';
+  else if (token.content == ")")
+    return ')';
+  else if (token.content == "[")
+    return '[';
+  else if (token.content == "]")
+    return ']';
+  else if (token.content == ".")
+    return '.';
+  else if (token.content == "&")
+    return '&';
+  else if (token.content == "!")
+    return '!';
+  else if (token.content == "~")
+    return '~';
+  else if (token.content == "-")
+    return '-';
+  else if (token.content == "+")
+    return '+';
+  else if (token.content == "*")
+    return '*';
+  else if (token.content == "/")
+    return '/';
+  else if (token.content == "%")
+    return '%';
+  else if (token.content == "<")
+    return '<';
+  else if (token.content == ">")
+    return '>';
+  else if (token.content == "^")
+    return '^';
+  else if (token.content == "|")
+    return '|';
+  else if (token.typeToString() == "IS")
+    return IS;
+  else if (token.typeToString() == "REVERSE")
+    return REVERSE;
+  else if (token.typeToString() == "WHILE")
+    return WHILE;
+  else if (token.typeToString() == "ROUTINE")
+    return ROUTINE;
+  else if (token.typeToString() == "END")
+    return END;
+  else if (token.typeToString() == "BREAK")
+    return BREAK;
+  else if (token.typeToString() == "RETURN")
+    return RETURN;
+  else if (token.typeToString() == "NOT")
+    return NOT;
+  else if (token.typeToString() == "XOR")
+    return XOR;
+  else if (token.typeToString() == "OR")
+    return OR;
+  else if (token.typeToString() == "AND")
+    return AND;
+  else if (token.typeToString() == "ELSE")
+    return ELSE;
+  else if (token.typeToString() == "THEN")
+    return THEN;
+  else if (token.typeToString() == "IF")
+    return IF;
+  else if (token.typeToString() == "IN")
+    return IN;
+  else if (token.typeToString() == "THEN")
+    return THEN;
+  else if (token.typeToString() == "FOR")
+    return FOR;
+  else if (token.typeToString() == "LOOP")
+    return LOOP;
+  else if (token.typeToString() == "ARRAY")
+    return ARRAY;
+  else if (token.typeToString() == "FALSE")
+    return FALSE;
+  else if (token.typeToString() == "TRUE")
+    return TRUE;
+  else if (token.typeToString() == "RECORD")
+    return RECORD;
+  else if (token.typeToString() == "TYPE")
+    return TYPE;
+  else if (token.typeToString() == "VAR")
+    return VAR;
+  else if (token.typeToString() == "INTEGER_LITERAL_KEYWORD")
+    return INTEGER_LITERAL_KEYWORD;
+  else if (token.typeToString() == "REAL_LITERAL_KEYWORD")
+    return REAL_LITERAL_KEYWORD;
+  else if (token.typeToString() == "BOOLEAN_LITERAL_KEYWORD")
+    return BOOLEAN_LITERAL_KEYWORD;
+  else if (token.typeToString() == "RANGE")
+    return RANGE;
+  else if (token.typeToString() == "CONTINUE")
+    return CONTINUE;
+  else if (token.typeToString() == "IDENTIFIER")
   {
-    yylval.id_val = strdup(tokenValue.c_str());
+    yylval.id_val = strdup(token.content.c_str());
     return IDENTIFIER;
   }
-  else if (tokenType == "INTEGER_LITERAL")
+  else if (token.typeToString() == "INTEGER_LITERAL")
   {
-    yylval.int_val = std::stoi(tokenValue);
+    yylval.int_val = std::stoi(token.content);
     return INTEGER_LITERAL;
   }
-  else if (tokenType == "REAL_LITERAL")
+  else if (token.typeToString() == "REAL_LITERAL")
   {
-    yylval.real_val = std::stoi(tokenValue);
+    yylval.real_val = std::stoi(token.content);
     return REAL_LITERAL;
   }
-  else if (tokenType == "BOOLEAN_LITERAL")
+  else if (token.typeToString() == "BOOLEAN_LITERAL")
   {
-    if (tokenValue == "true") 
-        yylval.bool_val = true;
-    else if (tokenValue == "false") 
-        yylval.bool_val = false;
+    if (token.content == "true")
+      yylval.bool_val = true;
+    else if (token.content == "false")
+      yylval.bool_val = false;
     return BOOLEAN_LITERAL;
   }
   return -1;
@@ -120,15 +176,14 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  FILE *input_file = fopen(argv[1], "r");
-  if (!input_file)
+  Lexer lexer(argv[1]);
+  tokens = *lexer.scan_code();
+  if (tokens.empty())
   {
-    perror("Error opening input file");
+    lexer.print_errors();
     return 1;
   }
 
-  tokenFile.open(argv[1]);
   yyparse();
-  fclose(input_file);
   return 0;
 }
